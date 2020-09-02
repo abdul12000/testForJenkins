2037072d75e08a81df4fa85cdc1ab0be5d470768
abdulsalam12000@yahoo.com <abdulsalam12000@yahoo.com>
2020-09-02 22:27:36 +0100

    Updated TestRunner Class

--- src/test/java/step_defintions/Testrunner.java
+++ src/test/java/step_defintions/Testrunner.java
@@ -5,6 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/resources/feature_files", plugin = {"pretty", "html:target/Myreport"}, tags="@TestRun")
+@CucumberOptions(features = "src/main/resources/feature_files",
+                plugin = {
+                        "pretty",
+                        "html:target/cucumber.html",
+                        "json:target/cucumber/cucumber-report.json",
+                        "junit:target/cucumber.xml"
+                },
+        tags = "@TestRun")
 public class Testrunner {
 }
