8fe698c71f8bfb693b7bb001244fa2e26b15a09d
abdulsalam12000@yahoo.com <abdulsalam12000@yahoo.com>
2020-09-02 22:59:22 +0100

    Updated TestRunner Class again2

--- src/test/java/step_defintions/Testrunner.java
+++ src/test/java/step_defintions/Testrunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/main/resources/feature_files",
+        monochrome = true,
                 plugin = {
                         "pretty",
                         "html:target/cucumber.html",
